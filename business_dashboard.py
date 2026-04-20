499ab9b3b04fa36094daf0f7442ad40b5e24fead
kfwshum <kfwshum@connect.ust.hk>
2026-04-20 17:46:39 +0800

    Update business_dashboard.py

--- business_dashboard.py
+++ business_dashboard.py
@@ -5,3 +5,8 @@ st.write(12345)
 st.write({"Name": "Alice", "Age": 30})
 st.write("**Bold Text** and *Italic Text*")
 st.header("Section 1: Introduction")
+age = st.number_input("Enter your age:",
+                      min_value=0,
+                      max_value=120,
+                      value=25)
+st.write(f"Your age is {age}")
